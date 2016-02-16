f641acde4148dc4b01018cfd8db3963f2ff4c471
Andres Spitzer <andres@grupowerk.com>
2016-02-16 13:35:37 +0100

    Package config

--- src/config/config.php
+++ src/config/config.php
@@ -1,7 +1,10 @@
 <?php
 return array(
-    'sepa_settings' => array(
-        'company',
-
+    'sepa_settings' => array("name" => "Bluemove",
+        "IBAN" => "NL50BANK1234567890",
+        "BIC" => "BANKNL2A",
+        "batch" => true,
+        "creditor_id" => "00000",
+        "currency" => "EUR"
     )
 );
